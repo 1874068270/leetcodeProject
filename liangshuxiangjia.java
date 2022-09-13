e05f61d26c0f70894d1cfd1b5bd132d1133c8eb4
root <root@localhost.localdomain>
2022-09-13 21:02:21 +0800

    add. 第二天

---
+++ liangshuxiangjia.java
@@ -0,0 +1,12 @@
+public class liangshuxiangjia {
+    public static void main(String[] args) {
+        System.out.println("niubiya111111122222");
+        ListNode root = new ListNode(0);
+        //ListNode test(0);
+
+
+        System.out.println(root.val);
+        //System.out.println(test.val);
+
+    }
+}
--- testJava.java
+++ testJava.java
@@ -1,14 +1,8 @@
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+
 public class testJava {
     public static void main(String[] args) {
         System.out.println("niubiya111111122222");
         ListNode test;
-        
+
     }
 }
